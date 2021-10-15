4487b78e863556057fb4acd5ff5bfabf9999bd93
Vineyo <41120222+Vineyo@users.noreply.github.com>
2021-10-15 22:52:41 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,22 +1,20 @@
-import taichi
 from Taichi_Moxi import *
 import random
 res=512
-moxi=Taichi_Moxi(res)
+moxi=Taichi_Moxi()
 
 gui=ti.GUI("taichi_moxi",(res,res))
-cursor=ti.Vector([0.5,0.5])
 moxi.setCurrentColor(0.9, 0.9, 0.25,0.5)
-
+moxi.setPigmentFlowRate(2)
+moxi.set_edge_parameters(-0.1,0.4,0.5)
+# moxi.Water_Surface[50,50]=1.0
 while gui.running:
     gui.get_event()
 
     if(gui.is_pressed(ti.GUI.RMB)):
-        
         moxi.setCurrentColor(random.random(),random.random(),random.random(),random.random())
+        
     if(gui.is_pressed(ti.GUI.LMB)):
-        # cursor[0] = gui.get_cursor_pos()[0]
-        # cursor[1] = gui.get_cursor_pos()[1]
         moxi.setCursor(gui.get_cursor_pos()[0],gui.get_cursor_pos()[1])
         moxi.drawStrok()
     moxi.update()
